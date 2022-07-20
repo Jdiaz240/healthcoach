539e866e2024be7ee29f6252bf8c8d8413a8de46
Joshua Diaz <jad24096@gmail.com>
2022-07-19 22:07:42 -0400

    created a react template to bring in previous vanilla html and css into.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import AboutMe from './components/AboutMe';
+
+function App() {
+  return (
+    <>
+    <Header />
+    <AboutMe />
+    <Footer />
+    </>
+  );
+}
+
+export default App;
